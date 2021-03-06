ca79b0c44d724512e719ba9d564260c0b89aa594
jinxul <ahmadgivekesh@gmail.com>
2021-03-06 19:56:21 +0330

    temporarily switch to api key instead of the access token

--- app/src/main/java/com/givekesh/movies/data/source/remote/NetworkApi.kt
+++ app/src/main/java/com/givekesh/movies/data/source/remote/NetworkApi.kt
@@ -2,17 +2,18 @@ package com.givekesh.movies.data.source.remote
 
 import com.givekesh.movies.data.model.OnTheaters
 import com.givekesh.movies.data.model.Movies
+import com.givekesh.movies.utils.Constants
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NetworkApi {
-    @GET("movie/popular")
+    @GET("movie/popular?api_key=${Constants.API_KEY}")
     suspend fun fetchPopularMovies(): Movies
 
-    @GET("movie/top_rated")
+    @GET("movie/top_rated?api_key=${Constants.API_KEY}")
     suspend fun fetchTopRatedMovies(): Movies
 
-    @GET("movie/now_playing")
+    @GET("movie/now_playing?api_key=${Constants.API_KEY}")
     suspend fun fetchOnTheaters(): OnTheaters
 
     @GET("/search/movie")
--- app/src/main/java/com/givekesh/movies/utils/Constants.kt
+++ app/src/main/java/com/givekesh/movies/utils/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val IMAGE_URL = "http://image.tmdb.org/t/p/w185"
     const val TMDB_TOKEN = ""
+    const val API_KEY = ""
 }
